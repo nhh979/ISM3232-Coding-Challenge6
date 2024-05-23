451f20302262d97ac66005f9bff8fee0848c271f
Hieu Nguyen <hieuunguyenn979@gmail.com>
2024-05-22 22:31:28 -0400

    Implement animation using setInterval.

--- ball_constructor.js
+++ ball_constructor.js
@@ -22,16 +22,43 @@ class Ball {
     // Create a method to update the ball's positions
     update (width, height) {
         // This method takes two arguments: canvan's width and canvas's height
-        if (this.x <= 0 || this.x > width){
+        if (this.x - this.radius < 0 || this.x + this.radius > width){
             this.dx *= -1
         }
-        if (this.y <= 0 || this.y > height){
+        if (this.y - this.radius < 0 || this.y + this.radius > height){
             this.dy *= -1
         }
+
         // Update the x, y coordinates
         this.x += this.dx;
         this.y += this.dy;
     }
 }
+
+// Get canvas and context
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+// Get the wideth and the height of the canvas
+const width = canvas.width
+const height = canvas.height
+
+// Create a function to draw the canvas's boundaries
+function canvasBoundary (){
+    ctx.strokeStyle = 'black';
+    ctx.strokeRect(0, 0, width, height)
+}
+// Create the ball object
+let ball = new Ball(20, 30, 20, 3, 2, 'pink')        
+
+// Implement animation
+setInterval(() => {
+    ctx.clearRect(0, 0, width, height);     // Clear the Canvas
+    canvasBoundary();                   // Draw the boundary
+    ball.draw(ctx);                     // Draw the ball
+    ball.update(width, height);         // Update the positions
+}, 
+10)    // Update the canvas every 10ms
+
 
 
