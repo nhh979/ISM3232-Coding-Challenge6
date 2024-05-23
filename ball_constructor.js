3c50212d883905fed9dde0cc0dec710361ddb620
Hieu Nguyen <hieuunguyenn979@gmail.com>
2024-05-22 21:44:01 -0400

    Creat Ball constructor with initial properties.

--- ball_constructor.js
+++ ball_constructor.js
@@ -1,19 +1,13 @@
-
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
-
-// Get the wideth and the height of the canvas
-let width = canvas.width
-let height = canvas.height
-
-// Initiate x,y-coordinates
-let x = 0, y = 0;
-
-// Define a function to draw a ball
-function drawBall (x, y) {
-    ctx.fillStyle = 'pink';
-    ctx.beginPath();
-    ctx.arc(x, y, 20, 0, Math.PI*2, false);
-    ctx.fill()
+// Create Ball class
+class Ball {
+    constructor (x, y, radius, dx, dy, color){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.dx = dx;
+        this.dy = dy;
+        this.color = color;
+    }
 }
+
 
