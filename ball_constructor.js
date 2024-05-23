ae5555867e5cacc5a19166253ba38dc37491af03
Hieu Nguyen <hieuunguyenn979@gmail.com>
2024-05-22 21:49:16 -0400

    Implement draw method for Ball class.

--- ball_constructor.js
+++ ball_constructor.js
@@ -1,3 +1,4 @@
+
 // Create Ball class
 class Ball {
     constructor (x, y, radius, dx, dy, color){
@@ -8,6 +9,17 @@ class Ball {
         this.dy = dy;
         this.color = color;
     }
+
+    // Create a method to draw the ball
+    draw (ctx){
+        // This method takes the context as an argument
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+        ctx.fill()
+    }
 }
+
+
 
 
